a8b2734f8315aee4387cbd23fdd235a2361c102b
connor worthen <connorw220@protonmail.com>
2019-11-09 06:35:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,3 +4,19 @@ def line(array)
   if array.count == 0
     puts "The line is currently emtpy"
   else
+    response = "The line is currently:"
+      array.each_with_index do |name, index|
+         response += " #{index+1}. #{name}" 
+   end
+
+   def now_serving(array)
+     if array.count == 0
+       puts "There is nobody waiting to be served!"
+     else
+       response = "The line is currently serving:"
+       array.each_with_index do |index, name|
+         response += " #{index+1. name}"
+      end
+    end
+         
+         
